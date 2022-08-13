2b61ff54006ac34abb1c69fb84e71032ef0d2f4c
jpcortesgd <jpcortesg@eafit.edu.co>
2022-08-13 18:31:38 -0500

    Set up client

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import { FaCss3Alt } from "react-icons/fa";
+import "./home.css";
+
+export default function home() {
+  return (
+    <div>
+      <h1 className="as">Home</h1>
+      <FaCss3Alt />
+    </div>
+  );
+}
---
+++ client/src/pages/register/Register.jsx
@@ -0,0 +1,10 @@
+import { FaCss3Alt } from "react-icons/fa";
+import "./register.css";
+
+export default function Register() {
+  return (
+    <div>
+      <h1>Register</h1>
+    </div>
+  );
+}
