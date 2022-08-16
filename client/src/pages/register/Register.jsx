73576eb9753b3fa83ceb3f5d1f86b07a3523a29f
unknown <mvillegas2@eafit.edu.co>
2022-08-16 01:11:45 -0500

    Se agrego la pagina de registro y la pagina principal

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -3,8 +3,18 @@ import "./register.css";
 
 export default function Register() {
   return (
-    <div>
-      <h1>Register</h1>
+<div>
+      <section className="form-register">
+        <h4>Registro</h4>
+        <input className="boton" type="text" name="nombres" id="nombres" placeholder="Ingrese su nombre" />
+        <input className="boton" type="text" name="apellidos" id="apellidos" placeholder="Ingrese su apellido" />
+        <input className="boton" type="email" name="correo" id="correo" placeholder="Ingrese su correo" />
+        <input className="boton" type="password" name="contraseña" id="contraseña" placeholder="Ingrese su contraseña" />
+        <p>Estoy de acuerdo con <a href="#">Terminos y Condiciones</a></p>
+        <p> </p>
+        <a href="http://localhost:5173/"><input className="boton" type="submit" value="Registrar"/></a>
+        <p><a href="#">Ya tienes una cuenta?</a></p>
+      </section>
     </div>
   );
 }
