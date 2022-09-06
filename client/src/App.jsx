8028e3411c3d3f9fa8c78af5eb8dc0e85e3e3d74
acarmonag <acarmonag@eafit.edu.co>
2022-09-06 09:36:51 -0500

    Cambios de union

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
+import Dashboard from './pages/supplier/Supplier.jsx';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </div>
