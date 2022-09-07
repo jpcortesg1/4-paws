36b79fa3a8cb454e951db1e9355e9222ccb68f4e
jpcortesgd <jpcortesg@eafit.edu.co>
2022-09-06 20:19:18 -0500

    Create register of user

--- api/person/serializers.py
+++ api/person/serializers.py
@@ -1,6 +1,18 @@
+from person.models import Person
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 
 # Serializer to validate the data for the person model
 class PersonSerializer(serializers.Serializer):
-    name = serializers.CharField(required=True)
+    name = serializers.CharField(max_length=100, min_length=5, required=True)
+    birth_date = serializers.DateField(required=True)
+    mobile = serializers.CharField(max_length=100, validators=[
+                                   UniqueValidator(queryset=Person.objects.all())])
+    email = serializers.EmailField(max_length=254, validators=[
+                                   UniqueValidator(queryset=Person.objects.all())])
+    phone = serializers.CharField(max_length=100, required=False)
+    password = serializers.CharField(
+        max_length=100, min_length=8, required=True)
+    notes = serializers.CharField(required=False)
+    address = serializers.CharField(max_length=100, required=True)
