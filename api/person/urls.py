2cdc1d3d7c409928e7ae57f3fe27259e8e041e3f
jpcortesgd <jpcortesg@eafit.edu.co>
2022-09-08 15:53:13 -0500

    Complete crud of person in api django

--- api/person/urls.py
+++ api/person/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import PersonView
 
 urlpatterns = [
-    path('persons/', PersonView.as_view(), name='persons'),
+    path('persons/', PersonView.as_view(), name='persons_list'),
+    path('persons/<int:id>', PersonView.as_view(), name='persons_process'),
 ]
