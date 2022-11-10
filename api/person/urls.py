f57ae7dc87f84948d0ee279035e227717d658b86
jpcortesgd <jpcortesg@eafit.edu.co>
2022-11-10 01:35:36 -0500

    Delete old api

--- api/person/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import PersonView
-
-urlpatterns = [
-    path('persons/', PersonView.as_view(), name='persons_list'),
-    path('persons/<int:id>', PersonView.as_view(), name='persons_process'),
-]
--- api/pet/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import PetView
-
-urlpatterns = [
-    path('pets/', PetView.as_view(), name='pets_list'),
-    path('pets/<int:id>', PetView.as_view(), name='pets_process'),
-]
