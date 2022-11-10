3319476c39f03191f50e9c9b8d9182b1957f8853
jpcortesgd <jpcortesg@eafit.edu.co>
2022-11-10 03:23:55 -0500

    Crud persons

--- api/persons/models.py
+++ api/persons/models.py
@@ -2,11 +2,26 @@ from django.db import models
 
 
 class Person(models.Model):
-    name = models.CharField(max_length=100)
-    birth_date = models.DateField()
-    mobile = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    role = models.CharField(max_length=100)
-    status
+    STATUS = (
+        ('P', 'Pending'),
+        ('A', 'Active'),
+        ('I', 'Inactive'),
+    )
+    ROLE = (
+        ('A', 'Admin'),
+        ('C', 'Customer'),
+        ('P', 'Provider'),
+        ('E', 'Employee')
+    )
+    name = models.CharField(max_length=100, null=False, blank=False)
+    birth_date = models.DateField(null=False, blank=False)
+    mobile = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    phone = models.CharField(max_length=100, blank=True, null=True)
+    password = models.CharField(max_length=100, null=False, blank=False)
+    role = models.CharField(max_length=1, choices=ROLE, blank=True, null=True)
+    status = models.CharField(
+        max_length=1, choices=STATUS, blank=True, null=True)
+    notes = models.TextField()
+    address = models.CharField(max_length=100)
+    score = models.FloatField(default=5)
