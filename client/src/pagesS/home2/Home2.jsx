6859984ffd1b1edf971f824ded654483b678ee90
acarmonag <acarmonag@eafit.edu.co>
2022-11-14 22:22:14 -0500

    beta 1.2

--- client/src/pagesS/home2/Home2.jsx
+++ client/src/pagesS/home2/Home2.jsx
@@ -10,10 +10,15 @@ const Home2 = () => {
             <Sidebar />
             <section className="home2Container">
                 <Navbar />
-                <a href="#home"><img id="profilepic" src="https://cartonlab.com/wp-content/uploads/2017/07/expositores-west-paw.jpg"  width="170" height="170" alt="profilepic"/></a>
-                <h1> 4Paws Store</h1>
-                <p id="paragarph1"> Everything you need 4 your dog<br/>
-                </p>
+                <div className="top">
+                    <a href="#home"><img id="profilepic" src="https://cartonlab.com/wp-content/uploads/2017/07/expositores-west-paw.jpg"  width="170" height="170" alt="profilepic"/></a>
+                </div>
+                <div className="midle">
+                    <h1> 4Paws Store</h1>
+                </div>
+                <div className="last">
+                    <p id="paragarph1"> Everything you need 4 your dog<br/></p>
+                </div>
              </section>
         </div>
     )
